69443aead4248df28a8aa2dbb51c0bb183981434
viggibot <33251437+viggibot@users.noreply.github.com>
2021-11-16 11:34:54 +0530

    Add files via upload

---
+++ src/Formfield.js
@@ -0,0 +1,20 @@
+import Header from "./header/header";
+import WorkExperience from "./header/workExperience";
+import Skills from "./header/skills";
+import Achivements from "./header/achivements";
+
+const UserDetails = () => {
+    return(
+        <div className="FormFields">
+            <h1 style={{textAlign:"center"}}>Resume Builder</h1>
+            <form>
+                <Header/>
+                <WorkExperience/>
+                <Achivements/>
+                <Skills/>
+            </form>
+        </div>
+    );
+}
+
+export default UserDetails;
