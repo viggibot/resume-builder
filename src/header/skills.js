54d96602b8b82d6d5d31772993c25f53c9cde321
viggibot <33251437+viggibot@users.noreply.github.com>
2021-11-16 18:42:15 +0530

    made some changes in skill

--- src/header/skills.js
+++ src/header/skills.js
@@ -2,26 +2,35 @@ import {useState} from "react";
 
 const Skills = () =>{
 
-    const skills = [];
+    const [pos, setpos] = useState(0)
 
-    const [tag, setTag] = useState(null);
+    const [skilltag, setTag] = useState(null);
+
+    const [tagName, setName] = useState("Skills");
 
     const addTag = (e) => {
         e.preventDefault();
-        console.log(e);
     }
+
+    console.log(skilltag);
+
+    
 
     return(
         <div className="WorkExperience common">
             <fieldset><legend><h2>SKILLS</h2></legend>
                 <div className="skill-tag">
                     <div className="inner-skill-container">
-                        <input type="text" placeholder="Ex: HTML" name="tags" required />
-                        <button type="submit">Add</button>
+                        <form onSubmit={(e) => addTag(e)}>
+                            <input type="text" placeholder="Ex: HTML" name="tag" onChange={(e) => setName(e.target.value)} value={tagName} required />
+                            <button type="submit">Add</button>
+                        </form>
                     </div>
                 </div>
             <div className="SkillsContainer">
-
+                <ul>
+                    {skilltag ? skilltag.map(element => <li key={pos}>{element.tag}{element.id}</li>) : null}
+                </ul>
             </div>
             </fieldset>
         </div>
